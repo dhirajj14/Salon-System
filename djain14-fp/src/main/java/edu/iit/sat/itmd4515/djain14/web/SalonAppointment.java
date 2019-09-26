825aa3e82ef0be426723f19a26c770f22046862e
Dhiraj Jain <dhirajj14@github.com>
2019-09-26 00:27:26 -0500

    Addin project to github repo

---
+++ djain14-fp/src/main/java/edu/iit/sat/itmd4515/djain14/web/SalonAppointment.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.iit.sat.itmd4515.djain14.web;
+
+import edu.iit.sat.itmd4515.djain14.domain.SalonCustomers;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+/**
+ *
+ * @author dhira
+ */
+@WebServlet(name = "SalonAppointment", urlPatterns = {"/SalonAppt"})
+public class SalonAppointment extends HttpServlet {
+
+    private static final Logger LOG = Logger.getLogger(SalonAppointment.class.getName());
+
+    @Resource
+    Validator validator;
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        LOG.info("In Doget");
+        SalonCustomers saloncustomers = new SalonCustomers();
+        request.setAttribute("saloncustomers", saloncustomers);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/newappointment.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String fullName = request.getParameter("fullName");
+        String address = request.getParameter("address");
+        String emailId = request.getParameter("emailId");
+        String contact = request.getParameter("contact");
+        String dateParam = request.getParameter("date");
+        String serviceType = request.getParameter("serviceType");
+        String time = request.getParameter("time");
+        LocalDate date = null;
+
+        if (dateParam != null && !(dateParam.isEmpty())) {
+            date = LocalDate.parse(dateParam);
+        }
+        SalonCustomers saloncustomers = new SalonCustomers(fullName, address, emailId, contact, date, serviceType, time);
+        LOG.info(saloncustomers.toString());
+
+        Set<ConstraintViolation<SalonCustomers>> constraintViolations = validator.validate(saloncustomers);
+
+        if (constraintViolations.size() > 0) {
+            LOG.info("we have a problem validating POJO");
+            for (ConstraintViolation<SalonCustomers> bad : constraintViolations) {
+                LOG.info(bad.getPropertyPath() + " " + bad.getMessage());
+            }
+            request.setAttribute("saloncustomers", saloncustomers);
+            request.setAttribute("mistakes", constraintViolations);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/newappointment.jsp");
+            dispatcher.forward(request, response);
+        } else {
+            LOG.info("We don't have any problem with this POJO");
+            request.setAttribute("saloncustomers", saloncustomers);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/appointmentconfirm.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
---
+++ djain14-fp/src/main/webapp/WEB-INF/appointmentconfirm.jsp
@@ -0,0 +1,41 @@
+<%-- 
+    Document   : appointmentconfirm
+    Created on : 25 Sep, 2019, 1:35:40 AM
+    Author     : dhira
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <div class="container" style=" margin-top: 2%;margin-left: 13%;margin-bottom: 2%;margin-right: 7%; ">
+            <div class="alert-success text-center">
+                <h1 class="display-3">Thank You!</h1>
+                <p class="lead"><strong>Your appointment has been booked with us.</strong></p>
+                <hr>
+                <br>
+                <h3 class="display-6">
+                    <strong>Your Booking Details </strong>
+                </h3>
+                <div style="color: black; padding:20px; ">
+                
+                <br>
+                <p class="lead"><strong>Name: </strong>${requestScope.saloncustomers.fullName}</p>
+                <p class="lead"><strong>Address: </strong>${requestScope.saloncustomers.address}</p>
+                <p class="lead"><strong>Email ID: </strong>${requestScope.saloncustomers.emailId}</p>
+                <p class="lead"><strong>Contact: </strong>${requestScope.saloncustomers.contact}</p>
+                <p class="lead"><strong>Service: </strong>${requestScope.saloncustomers.serviceType}</p>
+                <p class="lead"><strong>Date: </strong>${requestScope.saloncustomers.date}</p>
+                <p class="lead"><strong>Time: </strong>${requestScope.saloncustomers.time}</p>
+                </div></div></div>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
